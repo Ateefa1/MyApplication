30c3aa993c62b8f2d0709c05e5a1eb0a7fdbc1ab
Ateefa <ateefasiddiqui@gmail.com>
2019-09-24 17:41:53 +0500

    My application

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton = findViewById<Button>(R.id.Button)
+        val resultsTextView = findViewById<TextView>(R.id.textView3)
+        val seekbar = findViewById<SeekBar>(R.id.seekBar)
+
+        rollButton.setOnClickListener {
+          val rand = Random().nextInt(seekbar.progress)
+            resultsTextView.text = rand.toString()
+
+        }
+
+
+    }
+}
